3a76637ba5281ea12ec24cdecfe6524acda041ce
farrongoth <farrongoth@gmail.com>
2015-11-14 23:54:29 +0000

    Signed-off-by: farrongoth <farrongoth@gmail.com>

--- chatcommands.lua
+++ chatcommands.lua
@@ -1,39 +1,43 @@
-minetest.register_privilege("langton_ant","Can use langton_ant commands");
-minetest.register_privilege("langton_ant_global","Can use langton_ant commands");
-
 minetest.register_chatcommand("lant_start", {
 	params = "",
 	description = "Start langton_ant process",
-	privs = {langton_ant=true},
+	privs = {},
 	func = function(name,params)
+		if started then return end
+
 		local player = minetest.get_player_by_name(name)
 
 		if not player then
 			return false, "Player not found"
 		end
 
-		langton_ant.register_new_ant(player)
+		local pos = player:getpos()
+		local yaw = player:getyaw()
+
+		pos.x = math.floor(pos.x)
+		pos.y = math.floor(25.0) -- Height
+		pos.z = math.floor(pos.z)
+		yaw = 0.0
+
+		player:setpos(pos)
+		player:setyaw(yaw)
+
+		started = true
 	end
 })
 
 minetest.register_chatcommand("lant_stop", {
 	params = "",
 	description = "Stop langton_ant process",
-	privs = {langton_ant=true},
+	privs = {},
 	func = function(name,params)
+		if not started then return end
 		local player = minetest.get_player_by_name(name)
 
 		if not player then
 			return false, "Player not found"
 		end
-		langton_ant.unregister_ant(player)
-	end
-})
 
-minetest.register_chatcommand("lant_set", {
-	params = "",
-	description = "Stop langton_ant process",
-	privs = {langton_ant_global=true},
-	func = function(name,params)
+		started = false
 	end
 })
--- init.lua
+++ init.lua
@@ -1,72 +1,43 @@
-local path = minetest.get_modpath(minetest.get_current_modname())
+local timer = 0
+local step_timer = 0.25
+local direction = {x=0,y=0,z=0,yaw=0}
+local player_joined = false
 
-dofile(path .. "/chatcommands.lua")
-
-langton_ant = {}
-
-langton_ant.white = {name = "wool:white"}
-langton_ant.black = {name = "wool:black"}
-
-langton_ant.timer = 0
-langton_ant.step_timer = 0.05
-
-langton_ant.ants = {}
-
-langton_ant.register_new_ant = function(player)
-	-- Normalize player location
-	local pos = player:getpos()
-	local yaw = player:getyaw()
-
-	math.floor(pos.x)
-	math.floor(15.0)
-	math.floor(pos.z)
-	yaw = 0.0
-
-	player:setpos(pos)
-	player:setyaw(yaw)
-
-	langton_ant.ants[player:get_player_name()] = {direction = {x=0,y=1,z=0,yaw=0}, player = player}
-end
-
-langton_ant.update_ant = function(ant)
-	local pos = ant.player:getpos()
-	local node_pos = pos
-
-	node_pos.y = node_pos.y - 1
-
-	local node = minetest.get_node(node_pos)
-	local name = node.name
-	local dir = ant.direction
-
-	if name ~= langton_ant.white.name then
-		minetest.set_node(node_pos,langton_ant.white)
-		dir.yaw = dir.yaw - math.pi/2
-	else
-		minetest.set_node(node_pos,langton_ant.black)
-		dir.yaw = dir.yaw + math.pi/2
-	end
-
-	dir.x = math.cos(dir.yaw)
-	dir.z = math.sin(dir.yaw)
-
-	pos.x = pos.x + dir.x
-	pos.y = pos.y + dir.y
-	pos.z = pos.z + dir.z
-
-	ant.player:setpos(pos)
-end
-
-langton_ant.unregister_ant = function(player)
-	langton_ant.ants[player:get_player_name()] = nil
-end
+minetest.register_on_joinplayer(function(player)
+	player_joined = true
+end)
 
 minetest.register_globalstep(function(dt)
-	langton_ant.timer = langton_ant.timer + dt
+	if not player_joined then return end
 
-	if langton_ant.timer >= langton_ant.step_timer then
-		for key,val in pairs(langton_ant.ants) do
-			langton_ant.update_ant(val)
+	timer = timer + dt
+	if timer >= step_timer then
+		local player = minetest.get_player_by_name('singleplayer')
+		local pos = player:getpos()
+
+		local node_pos = {}
+		node_pos.x = pos.x
+		node_pos.y = pos.y - 1
+		node_pos.z = pos.z
+
+		local node = minetest.get_node(node_pos)
+		local name = node.name
+
+		if name ~= "wool:white" then
+			minetest.set_node(node_pos,{name="wool:white"})
+			direction.yaw = direction.yaw - math.pi/2
+		else
+			minetest.set_node(node_pos,{name="wool:black"})
+			direction.yaw = direction.yaw + math.pi/2
 		end
-		langton_ant.timer = 0
+
+		direction.x = math.cos(direction.yaw)
+		direction.z = math.sin(direction.yaw)
+
+		pos.x = pos.x + direction.x
+		pos.y = pos.y + direction.y
+		pos.z = pos.z + direction.z
+		player:setpos(pos)
+		timer = 0
 	end
 end)
